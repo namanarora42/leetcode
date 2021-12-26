805ea8d7dacadce37151ffcb5fe47b2a25860c75
Naman Arora <85018020+namanarora97@users.noreply.github.com>
2021-12-25 23:08:20 -0500

    Time: 146 ms (5.09%), Space: 115.8 MB (5.32%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int[][] res = new int[k][2];
+        Map<Double, List<int[]>> map = new TreeMap<>();
+        for (int[] each : points) {
+            double dist = distance(each);
+            if (map.containsKey(dist)) {
+                List<int[]> temp = map.get(dist);
+                temp.add(each);
+            }
+            else {
+                List<int[]> temp = new LinkedList<>();
+                temp.add(each);
+                map.put(dist, temp);
+            }
+        }
+        int added = 0;
+        ArrayList<Double> keys = new ArrayList<>(map.keySet());
+        while (added < k) {
+            List<int[]> temp = map.get(keys.remove(0));
+            for (int[] x : temp) {
+                res[added++] = x;
+            }
+        }
+        return res;
+    }
+    
+    public double distance(int[] x) {
+        int x1 = x[0]; 
+        int y1 = x[1];
+        int x2 = 0;
+        int y2 = 0;
+        
+        return Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+    }
+}
