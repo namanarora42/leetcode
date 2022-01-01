cc59ff929cc10755ca5f4a1c1a7fdb930faa7c5e
Naman Arora <85018020+namanarora97@users.noreply.github.com>
2021-12-31 19:13:16 -0500

    Time: 5 ms (28.28%), Space: 39.2 MB (34.09%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -12,14 +12,6 @@ class Solution {
         letters.put('7', Arrays.asList(new Character[]{'p', 'q', 'r', 's'}));
         letters.put('8', Arrays.asList(new Character[]{'t', 'u', 'v'}));
         letters.put('9', Arrays.asList(new Character[]{'w', 'x', 'y', 'z'}));
-        // letters.put('2', Arrays.asList("abc".toCharArray()));
-        // letters.put('3', Arrays.asList("def".toCharArray()));
-        // letters.put('4', Arrays.asList("ghi".toCharArray()));
-        // letters.put('5', Arrays.asList("jkl".toCharArray()));
-        // letters.put('6', Arrays.asList("mno".toCharArray()));
-        // letters.put('7', Arrays.asList("pqrs".toCharArray()));
-        // letters.put('8', Arrays.asList("tuv".toCharArray()));
-        // letters.put('9', Arrays.asList("wxyz".toCharArray()));
         for (char digit : digits.toCharArray()) {
             if (output.isEmpty()){
                 for (char c : letters.get(digit)) {
