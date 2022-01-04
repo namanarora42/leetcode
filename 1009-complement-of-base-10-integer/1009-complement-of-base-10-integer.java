dd36211acb40ac39f502fec92dfaddc9bbdde0b9
Naman Arora <85018020+namanarora97@users.noreply.github.com>
2022-01-04 16:17:30 -0500

    Time: 1 ms (16.42%), Space: 38 MB (7.20%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,15 +1,26 @@
 class Solution {
+    // public int bitwiseComplement(int n) {
+    //     String bin = Integer.toBinaryString(n);
+    //     StringBuilder res = new StringBuilder();
+    //     for (char c : bin.toCharArray()) {
+    //         if (c == '0') {
+    //             res.append('1');
+    //         }
+    //         else {
+    //             res.append('0');
+    //         }
+    //     }
+    //     return Integer.parseInt(res.toString(), 2);
+    // }
+    
     public int bitwiseComplement(int n) {
-        String bin = Integer.toBinaryString(n);
-        StringBuilder res = new StringBuilder();
-        for (char c : bin.toCharArray()) {
-            if (c == '0') {
-                res.append('1');
-            }
-            else {
-                res.append('0');
+        int res = 0;
+        char[] digits = Integer.toBinaryString(n).toCharArray();
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[digits.length - i - 1] == '0') {
+                res += Math.pow(2,i);
             }
         }
-        return Integer.parseInt(res.toString(), 2);
+        return res;
     }
 }
