2f3f8d9fec26663f83fde5f3b5e563cf15dc7575
Naman Arora <85018020+namanarora97@users.noreply.github.com>
2022-03-11 12:15:43 -0500

    Time: 7768 ms (5.62%), Space: 14 MB (96.12%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,21 +1,14 @@
 class Solution:
-    # def subArrayRanges(self, nums: List[int]) -> int:
-    #     res = 0
-    #     for i in range(len(nums)):
-    #         for j in range(i,len(nums)):
-    #             if j>=i:
-    #                 subarr = nums[i:j+1]
-    #                 r = max(subarr) - min(subarr)
-    #                 res += r
-    #     return res
+
 
     def subArrayRanges(self, A):
         res = 0
         n = len(A)
         for i in range(n):
-            l,r = A[i],A[i]
+            leftPtr = A[i]
+            rightPtr = A[i]
             for j in range(i, n):
-                l = min(l, A[j])
-                r = max(r, A[j])
-                res += r - l
+                leftPtr = min(leftPtr, A[j])
+                rightPtr = max(rightPtr, A[j])
+                res += rightPtr - leftPtr
         return res
